a599d878bdd1f71a3d60f6d8301830cb2b98fe6b
LEIGUO <tonyguolei@gmail.com>
2014-10-25 12:13:11 +0200

    test avec l'autre utilisateur git

--- Users/src/main/java/User.java
+++ Users/src/main/java/User.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+
 public class User {
 
     // les paremetres d'user
@@ -92,6 +93,7 @@ public class User {
     public void setSocket(Socket socket) {
         this.socket = socket;
     }
+
 
     public static void main(String[] args) throws Exception {
         User user = new User();
